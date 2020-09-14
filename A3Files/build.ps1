e50f0f40ffd6386682362a464dee8670eb0bfa65
Duckfine <daniel.gran@online.de>
2020-09-14 14:32:57 +0200

    Improved Build Script

--- A3Files/build.ps1
+++ A3Files/build.ps1
@@ -3,35 +3,29 @@
 Clear-Host;
 
 $pboManager = "S:\Programme\PboManager\PBOConsole.exe"
+$Path = "P:\A3SERVER_Project\SERVER\DUCKLIFE"
 
 Set-Alias -Name PBO -Value $pboManager
 
 # Stop Arma 3 Server Instance(s)
 Stop-Process -Name arma3server_x64 -Force -Confirm -ErrorAction SilentlyContinue
 
-Remove-Item -Recurse -Force C:\SHARE\A3Master
+Remove-Item -Recurse -Force C:\SHARE\A3Master -ErrorAction SilentlyContinue
 
-Remove-Item -Path "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCKOTH\addons\duckoth.pbo"
-Remove-Item -Path "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCK.Core\addons\duck.core.pbo"
-Remove-Item -Path "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCK.Life\addons\duck.life.pbo"
+Remove-Item -Path "$($Path)\A3Master\@DUCKOTH\addons\duckoth.pbo"
+Remove-Item -Path "$($Path)\A3Master\@DUCK.Core\addons\duck.core.pbo"
+Remove-Item -Path "$($Path)\A3Master\@DUCK.Life\addons\duck.life.pbo"
 
-PBO -pack "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCKOTH\addons\duckoth" "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCKOTH\addons\duckoth.pbo"
-PBO -pack "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCK.Core\addons\duck.core" "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCK.Core\addons\duck.core.pbo"
-PBO -pack "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCK.Life\addons\duck.life" "P:\A3SERVER_Project\SERVER\KOTH\A3Master\@DUCK.Life\addons\duck.life.pbo"
+PBO -pack "$($Path)\A3Master\@DUCKOTH\addons\duckoth" "$($Path)\A3Master\@DUCKOTH\addons\duckoth.pbo"
+PBO -pack "$($Path)\A3Master\@DUCK.Core\addons\duck.core" "$($Path)\A3Master\@DUCK.Core\addons\duck.core.pbo"
+PBO -pack "$($Path)\A3Master\@DUCK.Life\addons\duck.life" "$($Path)\A3Master\@DUCK.Life\addons\duck.life.pbo"
 
-
-$Source = "P:\A3SERVER_Project\SERVER\KOTH"
 $Destination = "\\Dedcli-pcdaniel\share"
 
-
-Get-ChildItem -Path "$($Source)\*" | ForEach-Object {
-  
-  
+Get-ChildItem -Path "$($Path)\*" | % {
   if (($_.FullName) -NotLike '*vscode*') { 
-
     Write-Host "Copying $($_.Fullname) to $($Destination)"
     Copy-Item $_.fullname "$Destination" -Recurse -Force -Exclude *.log
-
   }; 
 }
 
