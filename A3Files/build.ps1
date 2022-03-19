3bb18900ae0fb933f6b24b72dc0e925715eb399e
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2022-03-19 21:28:15 +0100

    Refactoring (kind of, ugly script)

--- A3Files/build.ps1
+++ A3Files/build.ps1
@@ -1,34 +1,37 @@
-#Build A3 Server with PBOMANAGER
-
+# Build A3 Server with PBOManager
 Clear-Host;
 
-$pboManager = "S:\Programme\PboManager\PBOConsole.exe"
-$Path = "P:\A3SERVER_Project\SERVER\DUCKLIFE"
+$PBOManagerExecutable = "S:\Programme\PBOManagerExecutable\PBOConsole.exe"
+$sourceCodePath = "P:\A3SERVER_Project\SERVER\DUCKLIFE"
+$Destination = "\\Dedcli-pcdaniel\share"
 
-Set-Alias -Name PBO -Value $pboManager
+$serverPath = "P:\a3servertest"
+
+
+Set-Alias -Name PBO -Value $PBOManagerExecutable
 
 # Stop Arma 3 Server Instance(s)
 Stop-Process -Name arma3server_x64 -Force -Confirm -ErrorAction SilentlyContinue
 
 Remove-Item -Recurse -Force C:\SHARE\A3Master -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$($Path)\A3Master\@DUCKOTH\addons\duckoth.pbo"
-Remove-Item -Path "$($Path)\A3Master\@DUCK.Core\addons\duck.core.pbo"
-Remove-Item -Path "$($Path)\A3Master\@DUCK.Life\addons\duck.life.pbo"
+Remove-Item -Path "$($sourceCodePath)\A3Master\@DUCKOTH\addons\duckoth.pbo"
+Remove-Item -Path "$($sourceCodePath)\A3Master\@DUCK.Core\addons\duck.core.pbo"
+Remove-Item -Path "$($sourceCodePath)\A3Master\@DUCK.Life\addons\duck.life.pbo"
 
-PBO -pack "$($Path)\A3Master\@DUCKOTH\addons\duckoth" "$($Path)\A3Master\@DUCKOTH\addons\duckoth.pbo"
-PBO -pack "$($Path)\A3Master\@DUCK.Core\addons\duck.core" "$($Path)\A3Master\@DUCK.Core\addons\duck.core.pbo"
-PBO -pack "$($Path)\A3Master\@DUCK.Life\addons\duck.life" "$($Path)\A3Master\@DUCK.Life\addons\duck.life.pbo"
+PBO -pack "$($sourceCodePath)\A3Master\@DUCKOTH\addons\duckoth" "$($sourceCodePath)\A3Master\@DUCKOTH\addons\duckoth.pbo"
+PBO -pack "$($sourceCodePath)\A3Master\@DUCK.Core\addons\duck.core" "$($sourceCodePath)\A3Master\@DUCK.Core\addons\duck.core.pbo"
+PBO -pack "$($sourceCodePath)\A3Master\@DUCK.Life\addons\duck.life" "$($sourceCodePath)\A3Master\@DUCK.Life\addons\duck.life.pbo"
 
-$Destination = "\\Dedcli-pcdaniel\share"
 
-Get-ChildItem -Path "$($Path)\*" | % {
+Get-ChildItem -Path "$($sourceCodePath)\*" | % {
   if (($_.FullName) -NotLike '*vscode*') { 
     Write-Host "Copying $($_.Fullname) to $($Destination)"
     Copy-Item $_.fullname "$Destination" -Recurse -Force -Exclude *.log
   }; 
 }
 
-Copy-Item "C:\SHARE\*" -Destination "P:\a3servertest" -Recurse -Force
+Copy-Item "C:\SHARE\*" -Destination $serverPath -Recurse -Force
+
 
 P:\a3servertest\A3Master\arma3server_x64_Start.bat
