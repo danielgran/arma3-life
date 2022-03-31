85ca798757c5b04978882c105ba6cb64ded43689
Daniel Gran <contact@grandaniel.com>
2022-03-31 19:12:13 +0200

    Renamed file

--- preprocess-servermods.sh
+++ preprocess-servermods.sh
@@ -6,6 +6,8 @@ mkdir dist
 for servermod in $(find ./src/servermods -mindepth 1 -maxdepth 1 -type d); do
   servermod_path=$(realpath $servermod)
   servermod_name=$(basename $servermod_path)
+
+  
   
   # Check for valid mod.cpp
   if [ ! -f "$servermod_path/mod.cpp" ]; then
