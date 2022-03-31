0168b9261916c50f338c6de1b1565e9f32902e94
Daniel Gran <contact@grandaniel.com>
2022-03-31 20:45:53 +0200

    Added client preprocess script

--- preprocess-clientmissions.sh
+++ preprocess-clientmissions.sh
@@ -1,10 +1,20 @@
 #/bin/bash
 
+
+# preprocesses the sqf files and the descripton.ext file
+
 for clientmission in $(find ./src/clientmissions -mindepth 1 -maxdepth 1 -type d); do
   clientmission_path=$(realpath $clientmission)
   clientmission_name=$(basename $clientmission_path)
 
   echo $clientmission_name
 
+
+  
+  source_root="$(pwd)/src/clientmissions/$clientmission_name"
+  target_root="$(pwd)/dist/mpmissions/$clientmission_name"
+  mkdir -p $target_root
+
+  cpp -I$source_root -P ./src/clientmissions/$clientmission_name/description.ext $target_root/description.ext
 
 done
